a982c64c618515aefbcebb45d8aaf8b623365618
MOHD FAIZAN <147900762+muhammadfaizan1641@users.noreply.github.com>
2025-05-29 22:40:30 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,17 +1,13 @@
 const { Router } = require("express");
 const adminRouter = Router();
-const { adminModel, courseModel } = require("../db");
+const { adminModel, courseModel } = require("./db");
 const jwt = require("jsonwebtoken");
-// brcypt, zod, jsonwebtoken
 const  { JWT_ADMIN_PASSWORD } = require("../config");
-const { adminMiddleware } = require("../middleware/admin");
+const { adminMiddleware } = require("./adminMiddleware");
 
 
 adminRouter.post("/signup", async function(req, res) {
-    const { email, password, firstName, lastName } = req.body; // TODO: adding zod validation
-    // TODO: hash the password so plaintext pw is not stored in the DB
-
-    // TODO: Put inside a try catch block
+    const { email, password, firstName, lastName } = req.body; 
     await adminModel.create({
         email: email,
         password: password,
@@ -26,8 +22,6 @@ adminRouter.post("/signup", async function(req, res) {
 
 adminRouter.post("/signin", async function(req, res) {
     const { email, password } = req.body;
-
-    // TODO: ideally password should be hashed, and hence you cant compare the user provided password and the database password
     const admin = await adminModel.findOne({
         email: email,
         password: password
@@ -55,7 +49,6 @@ adminRouter.post("/course", adminMiddleware, async function(req, res) {
 
     const { title, description, imageUrl, price } = req.body;
 
-    // creating a web3 saas in 6 hours
     const course = await courseModel.create({
         title: title, 
         description: description, 
@@ -74,8 +67,6 @@ adminRouter.put("/course", adminMiddleware, async function(req, res) {
     const adminId = req.userId;
 
     const { title, description, imageUrl, price, courseId } = req.body;
-
-    // creating a web3 saas in 6 hours
     const course = await courseModel.updateOne({
         _id: courseId, 
         creatorId: adminId 
