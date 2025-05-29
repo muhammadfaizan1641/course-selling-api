b75604ce4274605ff8819153402c7dbb8e96c518
MOHD FAIZAN <147900762+muhammadfaizan1641@users.noreply.github.com>
2025-05-29 15:18:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+console.log(process.env.MONGO_URL)
+const express = require("express");
+const mongoose = require("mongoose");
+
+const { userRouter } = require("./routes/user");
+const { courseRouter } = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
+const app = express();
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+    res.json({
+        message:"API is working fine"
+    })
+})
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/course", courseRouter);
+app.use((req,res)=>{
+    res.status(404).send({
+        message: "No such endpoint"
+    })
+})
+
+async function main() {
+    await mongoose.connect(process.env.MONGO_URL)
+    app.listen(3000);
+    console.log("listening on port 3000")
+}
+
+main()
