2dce999461017d5042fe43fb8965610f3e3cd4cd
MOHD FAIZAN <147900762+muhammadfaizan1641@users.noreply.github.com>
2025-05-29 22:54:03 +0530

    Update adminMiddleware.js

--- adminMiddleware.js
+++ adminMiddleware.js
@@ -1,21 +1,6 @@
 const jwt = require("jsonwebtoken");
-const { JWT_ADMIN_PASSWORD } = require("../config");
+const { JWT_ADMIN_PASSWORD } = require("./config");
 
-// function middleware(password) {
-//     return function(req, res, next) {
-//         const token = req.headers.token;
-//         const decoded = jwt.verify(token, password);
-
-//         if (decoded) {
-//             req.userId = decoded.id;
-//             next()
-//         } else {
-//             res.status(403).json({
-//                 message: "You are not signed in"
-//             })
-//         }    
-//     }
-// }
 
 function adminMiddleware(req, res, next) {
     const token = req.headers.token;
